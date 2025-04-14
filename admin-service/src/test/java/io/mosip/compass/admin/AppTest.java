214950a8f3dc056a71f934389f1a03ec334a7a64
Piyush7034 <piyushshukla2100@gmail.com>
2025-04-14 08:36:23 +0530

    Some refactoring added
    Signed-off-by: Piyush7034 <piyushshukla2100@gmail.com>

--- admin-service/src/test/java/io/mosip/compass/admin/AppTest.java
+++ admin-service/src/test/java/io/mosip/compass/admin/AppTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class MosipIomAdminServerTest
+public class AppTest 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class MosipIomAdminServerTest
      *
      * @param testName name of the test case
      */
-    public MosipIomAdminServerTest(String testName )
+    public AppTest( String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class MosipIomAdminServerTest
      */
     public static Test suite()
     {
-        return new TestSuite( MosipIomAdminServerTest.class );
+        return new TestSuite( AppTest.class );
     }
 
     /**
